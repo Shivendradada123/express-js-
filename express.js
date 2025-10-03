1706e39a3aed03e90e19d9316b2b68e7801cf93f
Shivedra123 <shivendrasaket2580@gmail.com>
2025-10-03 16:04:19 +0530

    express code

--- express.js
+++ express.js
@@ -1,28 +1,35 @@
-const http = require('http')
 
 const express = require('express');
 
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use( (req, res, next) => {
     console.log("this is a first request", req.url, req.method);
+    //  res.send("are this a express ");
  next();
 });
-app.use((req, res, next) => {
+
+app.use("/s", (req, res, next) => {
+    console.log("this is a first request", req.url, req.method);
+     res.send("are this a express code");
+
+});
+
+app.use("/shivendra", (req, res, next) => {
     console.log("this is a Second request", req.url, req.method);
 
     res.send("shivendra mauhariya");
+   
 });
 
 
 
 
-const server = http.createServer(app);
 
 const PORT = 5000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`server is runinng on  adress http://localhost:${PORT}`)
 });
 
